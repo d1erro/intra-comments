1788f91f20e557489f6d4f13116a1d6c70971e24
Damir Gimazetdinov <d1erro7@gmail.com>
2022-07-29 20:58:25 +0300

    Improve

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button');
+const button = document.querySelector('#comments');
 
 button.addEventListener('click', (e) => {
     let inputNumbers = document.getElementById('numbers').value;
@@ -6,4 +6,9 @@ button.addEventListener('click', (e) => {
     for (let i = 0; i < arrayNumbers.length; i++) {
         window.open(`https://admin-nk.tatar.ru/opengov/order/${arrayNumbers[i]}`);
     }
+})
+
+const statistic = document.querySelector('#statistic');
+statistic.addEventListener('click', () => {
+    window.open(`https://admin-nk.tatar.ru/opengov/statistics/index`);
 })
