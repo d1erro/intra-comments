bcff8bddd3816bf320e7124dfc7e19474b81d137
Damir Gimazetdinov <d1erro7@gmail.com>
2022-07-31 13:26:32 +0300

    Add new functionality
    Added the ability to specify a template.
    There was a table of notices showing the number of comments.
    Now you can clear the list of notices and the table.

--- script.js
+++ script.js
@@ -1,14 +1,104 @@
-const button = document.querySelector('#comments');
+const openNoticesInNewTabButton = document.querySelector('#add-comments');
 
-button.addEventListener('click', (e) => {
-    let inputNumbers = document.getElementById('numbers').value;
-    let arrayNumbers = inputNumbers.split('\n');
-    for (let i = 0; i < arrayNumbers.length; i++) {
-        window.open(`https://admin-nk.tatar.ru/opengov/order/${arrayNumbers[i]}`);
+let uniqueArray = [];
+let arrayNotices = [];
+let currentTemplate = '';
+let radioButtonValue;
+
+const inputTemplate = document.querySelector('#template');
+inputTemplate.addEventListener('change', (e) => {
+    setCurrentTemplate(e.target.value);
+    printCurrentTemplate(currentTemplate);
+})
+
+const textArea = document.querySelector('textarea');
+textArea.addEventListener('change', () => {
+    const inputNumbers = document.getElementById('numbers').value;
+    arrayNotices = inputNumbers.split('\n');
+    uniqueArray = getUniqueElementsArray(arrayNotices);
+});
+
+openNoticesInNewTabButton.addEventListener('click', () => {
+    for (let i = 0; i < uniqueArray.length; i++) {
+        window.open(`${currentTemplate}${uniqueArray[i]}`);
+    }
+});
+
+const printNoticesButton = document.querySelector('#print-notices');
+printNoticesButton.addEventListener('click', () => {
+    for (let num of uniqueArray) {
+        printNotices(num);
+    }
+});
+
+const resetTextArea = document.querySelector('#clear-textarea');
+resetTextArea.addEventListener('click' , () => {
+    textArea.value = '';
+    uniqueArray = [];
+    arrayNotices = [];
+});
+
+const noticeList = document.querySelector('.notice-list');
+const secondColumn = document.querySelector('.second-column');
+
+const clearTableButton = document.querySelector('#clear-table');
+clearTableButton.addEventListener('click', () => {
+    const noticesInDisplay = document.querySelectorAll('.notice')
+    noticesInDisplay.forEach(notice => {
+        notice.remove();
+    })
+});
+
+noticeList.addEventListener('click', (e) => {
+    const notice = document.querySelector(`#${e.target.id}`);
+    if (!notice.classList.value.includes('done-notice')) {
+        notice.classList.remove('fail-notice');
+        notice.classList.add('done-notice');
+    } else {
+        notice.classList.remove('done-notice');
+        notice.classList.add('fail-notice');
     }
 })
 
-const statistic = document.querySelector('#statistic');
-statistic.addEventListener('click', () => {
-    window.open(`https://admin-nk.tatar.ru/opengov/statistics/index`);
+const getRadioButtons = document.querySelector('.switch-buttons');
+getRadioButtons.addEventListener('change', (e) => {
+
 })
+
+function getUniqueElementsArray(array) {
+    const result = [];
+    for (let item of array) {
+        if (!result.includes(item)) result.push(item);
+    }
+    return result;
+}
+
+function printNotices(notice) {
+    const createNotice = document.createElement('button');
+    createNotice.className = 'notice';
+    createNotice.innerText = `Заявка: ${notice} | Комментариев - ${amountNoticeInArray
+    (notice, arrayNotices)}`;
+    createNotice.id = `notice${notice}`;
+    noticeList.append(createNotice);
+}
+
+function amountNoticeInArray(notice, array) {
+    const result = array.filter(element => element === notice);
+    return result.length;
+}
+
+function printCurrentTemplate(str) {
+    const createDisplayCurrentTemplate = document.createElement('p');
+    createDisplayCurrentTemplate.innerText = `Текущий шаблон: ${str}`;
+    createDisplayCurrentTemplate.className = 'current-template';
+    const getDisplayCurrentTemplate = document.querySelector('.current-template')
+    if (getDisplayCurrentTemplate === null) {
+        inputTemplate.before(createDisplayCurrentTemplate);
+    } else {
+        getDisplayCurrentTemplate.replaceWith(createDisplayCurrentTemplate);
+    }
+}
+
+function setCurrentTemplate(str) {
+    currentTemplate = str;
+}
